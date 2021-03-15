894b7e88ddc050a367370534f3e4fcb50088742b
tim <doddt2@fcc.ca>
2021-03-15 11:31:45 -0600

    commit

--- src/main/java/ca/devpro/OnboardingMicroserviceApplication.java
+++ src/main/java/ca/devpro/OnboardingMicroserviceApplication.java
@@ -9,6 +9,15 @@ import org.springframework.context.support.MessageSourceAccessor;
 @SpringBootApplication
 public class OnboardingMicroserviceApplication {
 
+    //in intellij preference -> plugins add lombok plugin,
+    //also annotation processing must be turned on for all projects that use lombok.
+
+    //finish unit tests for user validator and user controller
+    //add validations for length of firstName, lastName username fields
+    //add unique username validation (userRepository.existsByUsername) done
+    //add a phone subresource (liquibase, entity, assembler, controller, tests etc) (look up jpa OneToMany)
+
+
     public static void main(String[] args) {
         SpringApplication.run(OnboardingMicroserviceApplication.class, args);
     }
