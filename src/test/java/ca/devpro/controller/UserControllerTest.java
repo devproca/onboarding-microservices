eb69f12ef6a1902df0375a2612cdfe3fd002f785
Colin <colin@dev-pro.ca>
2021-03-15 14:12:57 -0600

    Added unit tests for user controller.
    All basic CRUD operations tested for users.

--- src/test/java/ca/devpro/controller/UserControllerTest.java
+++ src/test/java/ca/devpro/controller/UserControllerTest.java
@@ -3,11 +3,14 @@ package ca.devpro.controller;
 import ca.devpro.api.UserDto;
 import ca.devpro.client.UserClient;
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
+
+import javax.ws.rs.NotFoundException;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "classpath:teardown.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
@@ -24,10 +27,12 @@ public class UserControllerTest {
         userClient.setBaseUri("http://localhost:" + port);
     }
 
+    // Create Tests
     @Test
     public void testCreate_whenValidUser_shouldCreateSuccessfully() {
         UserDto dto = newValidUserForCreate();
         UserDto createdDto = userClient.create(dto);
+
         assertNotNull(createdDto.getUserId());
     }
 
@@ -36,10 +41,64 @@ public class UserControllerTest {
         UserDto dto = newValidUserForCreate();
         UserDto createdDto = userClient.create(dto);
         UserDto getDto = userClient.get(createdDto.getUserId());
+
         assertEquals(createdDto, getDto);
     }
 
-    private UserDto newValidUserForCreate() {
+    @Test
+    public void testCreateAndUpdate_shouldCreateAndUpdateSuccessfully() { // Update Test
+        UserDto dto = newValidUserForCreate();
+        UserDto createdDto = userClient.create(dto);
+
+        assertNotNull(createdDto.getUserId());
+        createdDto.setFirstName("updateTest");
+        assertEquals(createdDto.getFirstName(), "updateTest");
+    }
+
+    @Test
+    public void testCreateAndDelete_shouldCreateAndDeleteUserSuccessfully() { // Delete Test
+        UserDto dto = newValidUserForCreate();
+        UserDto createdDto = userClient.create(dto);
+
+        assertNotNull(createdDto.getUserId());
+        userClient.delete(createdDto.getUserId());
+        assertThrows(NotFoundException.class, () -> userClient.get(createdDto.getUserId()));
+    }
+
+    // Find User(s) Tests
+    @Test
+    public void testFindSingleUser_shouldCreateAndFindUserSuccessfully() {
+        UserDto dto = newValidUserForCreate();
+        UserDto createdDto = userClient.create(dto);
+
+        assertNotNull(userClient.get(createdDto.getUserId()));
+    }
+
+    @Test
+    public void testFindAllUsers_shouldCreateAndFindAllUsersSuccessfully() {
+        UserDto dto = newValidUserForCreate();
+        UserDto createdDtoOne = userClient.create(dto);
+        UserDto createdDtoTwo = userClient.create(dto.setUsername("Test the Second"));
+        UserDto createdDtoThree = userClient.create(dto.setUsername("Test the Third"));
+
+        assertEquals(3, userClient.findAll().size());
+    }
+
+    @Test
+    public void testFindAll_afterDeletion_shouldCreateThenDeleteAndFindAllUsersSuccessfully() {
+        UserDto dto = newValidUserForCreate();
+        UserDto createdDtoOne = userClient.create(dto);
+        UserDto createdDtoTwo = userClient.create(dto.setUsername("Test the Second"));
+        UserDto createdDtoThree = userClient.create(dto.setUsername("Test the Third"));
+
+        assertEquals(3, userClient.findAll().size());
+        userClient.delete(createdDtoTwo.getUserId());
+        assertThrows(NotFoundException.class, () -> userClient.get(createdDtoTwo.getUserId()));
+        assertEquals(2, userClient.findAll().size());
+    }
+
+
+    private UserDto newValidUserForCreate() { // Test Data
         return new UserDto()
                 .setFirstName("test")
                 .setLastName("user")
