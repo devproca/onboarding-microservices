71ec6a2c477b676f45e1338d627f47aff90947a0
Colin <colin@dev-pro.ca>
2021-03-17 10:02:34 -0600

    Implemented better Twilio authentication.
    Changed Twilio implementation to initialize in separate file and pull auth token, account sid, and trial phone number from environmental variables instead of hard coded into VerificationService.

---
+++ src/main/java/ca/devpro/config/TwilioInit.java
@@ -0,0 +1,22 @@
+package ca.devpro.config;
+
+import com.twilio.Twilio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TwilioInit {
+    private final TwilioConfig twilioConfig;
+    private final static Logger LOGGER = LoggerFactory.getLogger(TwilioInit.class);
+
+    @Autowired
+    public TwilioInit(TwilioConfig twilioConfig) {
+        this.twilioConfig = twilioConfig;
+        Twilio.init(twilioConfig.getAccountSid(), twilioConfig.getAuthToken());
+
+        LOGGER.info("Twilio initialized ... with Account SID: {} ", twilioConfig.getAccountSid());
+        LOGGER.info("Twilio initialized ... with Sender Phone Number: {} ", twilioConfig.getTwilioNumber());
+    }
+}
--- src/main/java/ca/devpro/service/VerificationService.java
+++ src/main/java/ca/devpro/service/VerificationService.java
@@ -1,21 +1,25 @@
 package ca.devpro.service;
 
+import ca.devpro.config.TwilioConfig;
 import com.twilio.Twilio;
 import com.twilio.type.PhoneNumber;
 import com.twilio.rest.api.v2010.account.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
 @Service
 public class VerificationService {
-    public static final String AUTH_TOKEN = "858c4e1255e47c24de98ba59a93c6399"; // ENV variable these.
-    public static final String ACCOUNT_SID = "ACb907c199d7730e6e0f6da0a2f38a1ce0";
+
+    private TwilioConfig twilioConfig;
+
+    @Autowired
+    public VerificationService(TwilioConfig twilioConfig) { this.twilioConfig = twilioConfig; }
 
     public void sendVerifyCode(String phoneNumber, String verifyCode) {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         PhoneNumber recipient = new PhoneNumber(phoneNumber);
-        PhoneNumber twilioNumber = new PhoneNumber("+16029752728");
+        PhoneNumber twilioNumber = new PhoneNumber(twilioConfig.getTwilioNumber());
 
         Message.creator(recipient, twilioNumber, "Verification Code: " + verifyCode).create();
     }
