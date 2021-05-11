3e3fb228b2e9c5b12a47c07e78a861760f72ff93
Ali Essam <ali@dev-pro.ca>
2021-05-11 10:52:13 -0600

    few updates

--- src/main/java/ca/devpro/service/PhoneService.java
+++ src/main/java/ca/devpro/service/PhoneService.java
@@ -2,13 +2,17 @@ package ca.devpro.service;
 
 import ca.devpro.api.PhoneDto;
 import ca.devpro.assembler.PhoneAssembler;
+import ca.devpro.config.TwilioConfiguration;
 import ca.devpro.entity.Phone;
 import ca.devpro.exception.NotFoundException;
 import ca.devpro.repository.PhoneRepository;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.api.v2010.account.MessageCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -21,6 +25,13 @@ public class PhoneService {
     private PhoneRepository phoneRepository;
     @Autowired
     private PhoneValidator phoneValidator;
+    @Autowired
+    private static TwilioConfiguration twilioConfiguration;
+
+    @Autowired
+    public PhoneService(TwilioConfiguration twilioConfiguration){
+        this.twilioConfiguration = twilioConfiguration;
+    }
 
     public PhoneDto create(PhoneDto dto) {
         phoneValidator.validateAndThrow(dto);
@@ -59,21 +70,36 @@ public class PhoneService {
 
     public void initiateVerification(UUID phoneId) {
         Phone phone = phoneRepository.findById(phoneId).get();
-        String verificationKey = VerificationService.generateVerifyCode();
-
+        String verificationKey = createVerificationKey();
         phone.setVerificationKey(verificationKey);
         phoneRepository.save(phone);
+        initiateVerification(phone.getPhoneNumber());
+    }
 
-        verificationService.InitiateVerification(phone.getPhoneNumber(), verificationKey);
+    public static void initiateVerification(String phoneNumber) {
+        Phone from = new Phone();
+        from.setPhoneNumber(twilioConfiguration.getTwilioNumber());
+        Phone to = new Phone();
+        to.setPhoneNumber(phoneNumber);
+        String body = "Hi";
+        MessageCreator creator = Message.creator(to,
+                from,
+                body);
+        creator.create();
+
+    }
+
+    public static String createVerificationKey() {
+        Random rnd = new Random();
+        int key = rnd.nextInt(999999);
+        return String.format("%06d", key);
     }
 
     public void completeVerification(String verificationKey,  UUID phoneId) {
         Phone phone = phoneRepository.findById(phoneId).get();
-
         if (phone.getVerificationKey().equals(verificationKey)){
             phone.setIsVerified(true);
         }
-
         phoneRepository.save(phone);
     }
 
